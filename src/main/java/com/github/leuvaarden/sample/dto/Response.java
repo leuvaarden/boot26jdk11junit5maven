202853f3ad48629fd07a08be09a1909b8d0fe061
leuvaarden <probablyviolin@gmail.com>
2021-07-30 10:19:19 +0300

    Added response field ordering

--- src/main/java/com/github/leuvaarden/sample/dto/Response.java
+++ src/main/java/com/github/leuvaarden/sample/dto/Response.java
@@ -1,6 +1,9 @@
 package com.github.leuvaarden.sample.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public interface Response<T> {
+    @JsonProperty(index = 0)
     boolean getSuccess();
 
     default T getData() {
