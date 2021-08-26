620acf085b5a94e18733051f5a7ace1153d6a267
leuvaarden <probablyviolin@gmail.com>
2021-08-26 09:04:10 +0300

    Added empty test

---
+++ src/test/java/com/github/leuvaarden/sample/AnotherTest.java
@@ -0,0 +1,11 @@
+package com.github.leuvaarden.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AnotherTest {
+    @Test
+    void contextLoads() {
+    }
+}
